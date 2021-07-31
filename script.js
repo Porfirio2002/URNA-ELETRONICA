e90a0e37062e9e67573b79b4e2ce1abf6d567c31
Porfirio2002 <pedrohenriquedasilva014@gmail.com>
2021-07-31 16:20:33 -0300

    JS

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let seuVotoPara = document.querySelector('d1-1 span')
-let cargo = document.querySelector('d1-2 span');
-let geral = document.querySelector('d1-4');
-let direitaL = document.querySelector('d1-right');
-let digitos = document.querySelector('d1-3');
-let aviso = document.querySelector('d2');
+let seuVotoPara = document.querySelector('.d1-1 span')
+let cargo = document.querySelector('.d1-2 span');
+let geral = document.querySelector('.d1-4');
+let direitaL = document.querySelector('.d1-right');
+let digitos = document.querySelector('.d1-3');
+let aviso = document.querySelector('.d2');
 
 let etapaAtual = 0;
 let numero = '';
@@ -14,21 +14,36 @@ function comecarEtapa() {
     let numeroHtml = '';
 
     for(let i=0;i<etapa.numeros;i++) {
-        numeroHtml += '<div class="digito"></div>'
-    }
-    seuVotoPara
-    
-
+        if(i == 0) {
+            numeroHtml += '<div class="digito pisca"></div>';
+        } 
+        else {
+            numeroHtml += '<div class="digito"></div>'
+            }
+        }
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo
+    geral.innerHTML = '';
+    aviso.style.display = 'none';
+    direitaL.innerHTML = '';
+    digitos.innerHTML = numeroHtml;
 }
 function atualiza() {
-
+    
 }
 
 function clicou(n) {
    let digitoo = document.querySelector('.numero.pisca');
    if(digitoo !== null) {
        digitoo.innerHTML = n;
-       numero = `${numero}${n}` ;
+       numero = `${numero}${n}`;
+
+       digitoo.classList.remove('pisca');
+       if(digitoo.nextElementSibling !== null) {
+       digitoo.nextElementSibling.classList.add('pisca')
+    }else {
+        atualiza();
+    }
    }
 }
 function branco() {
